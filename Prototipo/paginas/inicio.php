69697130a24a4dd3517b8f06bb79f8dbee578f58
MiguelAngelSilva0108 <103139025+MiguelAngelSilva0108@users.noreply.github.com>
2023-04-27 16:00:30 -0600

    Add files via upload

--- Prototipo/paginas/inicio.php
+++ Prototipo/paginas/inicio.php
@@ -18,9 +18,9 @@
 
 
 <body>
-    <p class="Hola">texto de prueba en css</p>
+   
     <div>
-        <div id="carouselExampleDark" class="carousel carousel-dark slide carousel-full">
+        <div id="carouselExampleDark" class="carousel carousel-dark slide ">
             <div class="carousel-indicators">
                 <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active"
                     aria-current="true" aria-label="Slide 1"></button>
