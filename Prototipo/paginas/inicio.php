d26777f922d834758562df86a87bbca28a730055
MiguelAngelSilva0108 <103139025+MiguelAngelSilva0108@users.noreply.github.com>
2023-04-27 15:43:45 -0600

    Add files via upload

--- Prototipo/paginas/inicio.php
+++ Prototipo/paginas/inicio.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,18 +10,58 @@
 </head>
 <?PHP require('./navbar/navbar.php');
 ?>
-<link rel="stylesheet" href="./css/bootstrap.min.css"/>
+<link rel="stylesheet" href="./css/bootstrap.min.css" />
 <link rel="stylesheet" href="./css/navbar.css">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
 
-<!-- Dependencias de jQuery y Popper.js -->
+
 
 
 <body>
+    <p class="Hola">texto de prueba en css</p>
     <div>
-        <div class="img-container">
-            <img class="img-fluid"
-                src="" alt="..." />
+        <div id="carouselExampleDark" class="carousel carousel-dark slide carousel-full">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active"
+                    aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1"
+                    aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2"
+                    aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active" data-bs-interval="10000">
+                    <img src="https://assets.easybroker.com/property_images/743958/16818780/EB-BV3958.jpeg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>First slide label</h5>
+                        <p>Some representative placeholder content for the first slide.</p>
+                    </div>
+                </div>
+                <div class="carousel-item" data-bs-interval="2000">
+                    <img src="https://s3.amazonaws.com/assets.moveglobally.com/organization_files/20960/marca_inmobiliaria_inmuebles_ciduad_de_mexico_casas_departamentos_en_renta_venta_condominios_2.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Second slide label</h5>
+                        <p>Some representative placeholder content for the second slide.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="https://s3.amazonaws.com/assets.moveglobally.com/organization_files/20960/marca_inmobiliaria_inmuebles_ciduad_de_mexico_casas_departamentos_en_renta_venta_condominios_3.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Third slide label</h5>
+                        <p>Some representative placeholder content for the third slide.</p>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark"
+                data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark"
+                data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
         </div>
     </div>
 </body>
