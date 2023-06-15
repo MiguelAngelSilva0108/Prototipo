66e1b54b6922e33a789d166c8e812a7a8725c41d
MiguelAngelSilva0108 <103139025+MiguelAngelSilva0108@users.noreply.github.com>
2023-06-14 23:58:40 -0600

    Add files via upload

---
+++ Prototipo/paginas/servicios.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Servicios Unitarios</title>
+    <link rel="icon" href="https://cdn.freebiesupply.com/logos/large/2x/s-bahn-1-logo-png-transparent.png"
+        type="image/png">
+    <link rel="icon" href="img/logo.png" type="image/png">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap">
+</head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+<link rel="stylesheet" href="../css/servicios.css">
+<link rel="stylesheet" href="../css/navbar.css">
+<link rel="stylesheet" href="../css/footer.css">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+
+<body>
+    <?PHP require('../navbar/navbar.php'); ?>
+
+    <div class="container">
+        <div class="TextoTitulo">
+            <p class="Titulo">
+                Servicios unitarios
+            </p>
+            <p class="Descripcion">
+                Ofrecemos servicios únicos, sin necesidad de contratar un plan. Agenda tu cita, recuerda que la visita
+                de
+                nuestros colaboradores es completamente gratis. ¡Iremos lo más pronto posible!
+            </p>
+        </div>
+    </div>
+
+     <!--linea 1 de servicios -->
+  <div class="container imagenes">
+    <div class="flex-container">
+      <div class="flex-item">
+        <a href="/Prototipo/servicios/unitarios/explicacion.php?servicio=administracion">
+          <img src="https://cdn-icons-png.flaticon.com/512/217/217853.png" alt="administracion" class="iconos">
+          <p class="iconos-texto">Administración y recolección de recursos monetarios</p>
+        </a>
+      </div>
+      <div class="flex-item">
+        <a href="/Prototipo/servicios/unitarios/explicacion.php?servicio=albanileria">
+          <img src="https://cdn-icons-png.flaticon.com/512/2421/2421989.png" alt="albañil" class="iconos">
+          <p class="iconos-texto">Albañilería</p>
+        </a>
+      </div>
+      <div class="flex-item">
+        <a href="/Prototipo/servicios/unitarios/explicacion.php?servicio=camaras">
+          <img src="https://cdn-icons-png.flaticon.com/512/36/36311.png" alt="Vigilancia" class="iconos">
+          <p class="iconos-texto">Cámaras de vigilancia</p>
+        </a>
+      </div>
+      <div class="flex-item">
+        <a href="/Prototipo/servicios/unitarios/explicacion.php?servicio=comite">
+          <img src="https://cdn-icons-png.flaticon.com/512/4420/4420377.png" alt="comite" class="iconos">
+          <p class="iconos-texto">Comité de administración</p>
+        </a>
+      </div>
+    </div>
+  </div>
+
+<!--linea 2 de servicios -->
+<div class="container imagenes">
+    <div class="flex-container">
+
+    <div class="flex-item">
+        <a href="/Prototipo/servicios/unitarios/explicacion.php?servicio=plagas">
+          <img src="https://cdn-icons-png.flaticon.com/512/2636/2636843.png" alt="plagas"
+            class="iconos">
+          <p class="iconos-texto">Control de plagas</p>
+        </a>
+      </div>
+
+      <div class="flex-item">
+        <a href="/Prototipo/servicios/unitarios/explicacion.php?servicio=electricista">
+          <img src="https://cdn-icons-png.flaticon.com/512/1983/1983275.png" alt="electricista" class="iconos">
+          <p class="iconos-texto">Electricista</p>
+        </a>
+      </div>
+
+      <div class="flex-item">
+        <a href="/Prototipo/servicios/unitarios/explicacion.php?servicio=gobierno">
+          <img src="https://cdn-icons-png.flaticon.com/512/5605/5605882.png" alt="gobierno" class="iconos">
+          <p class="iconos-texto">Gestión de asuntos de gobierno y legales</p>
+        </a>
+      </div>
+
+      <div class="flex-item">
+        <a href="/Prototipo/servicios/unitarios/explicacion.php?servicio=impermeabilizacion">
+          <img src="https://cdn-icons-png.flaticon.com/512/129/129817.png" alt="Impermeabilización" class="iconos">
+          <p class="iconos-texto">Impermeabilización</p>
+        </a>
+      </div>
+    </div>
+  </div>
+
+  <!--linea 3 de servicios -->
+  <div class="container imagenes">
+    <div class="flex-container">
+
+      <div class="flex-item">
+        <a href="/Prototipo/servicios/unitarios/explicacion.php?servicio=cisternas">
+          <img src="https://cdn-icons-png.flaticon.com/512/3022/3022982.png" alt="cisternas" class="iconos">
+          <p class="iconos-texto">Lavado y desinfección de Cisternas</p>
+        </a>
+      </div>
+
+      <div class="flex-item">
+        <a href="/Prototipo/servicios/unitarios/explicacion.php?servicio=tinacos">
+          <img src="https://cdn-icons-png.flaticon.com/512/7608/7608679.png" alt="tinacos"
+            class="iconos">
+          <p class="iconos-texto">Lavado y desinfección de tinacos</p>
+        </a>
+      </div>
+
+      <div class="flex-item">
+        <a href="/Prototipo/servicios/unitarios/explicacion.php?servicio=albercas">
+          <img src="https://cdn-icons-png.flaticon.com/512/78/78682.png" alt="albercas"
+            class="iconos">
+          <p class="iconos-texto">Limpieza de Albercas</p>
+        </a>
+      </div>
+
+      <div class="flex-item">
+        <a href="/Prototipo/servicios/unitarios/explicacion.php?servicio=limpieza">
+          <img src="https://cdn-icons-png.flaticon.com/512/2866/2866900.png" alt="limpieza" class="iconos">
+          <p class="iconos-texto">Limpieza de áreas comunes</p>
+        </a>
+      </div>
+    </div>
+  </div>
+
+  <!--linea 4 de servicios -->
+  <div class="container imagenes">
+    <div class="flex-container">
+
+    <div class="flex-item">
+        <a href="/Prototipo/servicios/unitarios/explicacion.php?servicio=ventanales">
+          <img src="https://cdn-icons-png.flaticon.com/512/6410/6410650.png" alt="ventanales"
+            class="iconos">
+          <p class="iconos-texto">Limpieza de ventanales</p>
+        </a>
+      </div>
+
+      <div class="flex-item">
+        <a href="/Prototipo/servicios/unitarios/explicacion.php?servicio=plomeria">
+          <img src="https://cdn-icons-png.flaticon.com/512/4635/4635359.png" alt="plomeria" class="iconos">
+          <p class="iconos-texto">Plomería</p>
+        </a>
+      </div>
+
+      <div class="flex-item">
+        <a href="/Prototipo/servicios/unitarios/explicacion.php?servicio=arboles">
+          <img src="https://cdn-icons-png.flaticon.com/512/2174/2174132.png" alt="arboles" class="iconos">
+          <p class="iconos-texto">Poda y adorno de arboles</p>
+        </a>
+      </div>
+
+      <div class="flex-item">
+        <a href="/Prototipo/servicios/unitarios/explicacion.php?servicio=pasto">
+          <img src="https://cdn-icons-png.flaticon.com/512/7105/7105088.png" alt="pasto" class="iconos">
+          <p class="iconos-texto">Poda de pasto</p>
+        </a>
+      </div>
+    </div>
+  </div>
+
+  <!--linea 5 de servicios -->
+  <div class="container imagenes">
+    <div class="flex-container">
+     
+    <div class="flex-item">
+        <a href="/Prototipo/servicios/unitarios/explicacion.php?servicio=pintura">
+          <img src="https://cdn-icons-png.flaticon.com/512/1276/1276892.png" alt="pintura" class="iconos">
+          <p class="iconos-texto">Pintura Interior y Exterior</p>
+        </a>
+      </div>
+
+      <div class="flex-item">
+        <a href="/Prototipo/servicios/unitarios/explicacion.php?servicio=puertas">
+          <img src="https://cdn-icons-png.flaticon.com/512/2457/2457981.png" alt="puertas"
+            class="iconos">
+          <p class="iconos-texto">Puertas automáticas</p>
+        </a>
+      </div>
+
+      <div class="flex-item">
+        <a href="/Prototipo/servicios/unitarios/explicacion.php?servicio=pulido">
+          <img src="https://cdn-icons-png.flaticon.com/512/16/16386.png" alt="pulido" class="iconos">
+          <p class="iconos-texto">Pulido y limpieza de pisos</p>
+        </a>
+      </div>
+
+      <div class="flex-item">
+        <a href="/Prototipo/servicios/unitarios/explicacion.php?servicio=cuotas">
+          <img src="https://cdn-icons-png.flaticon.com/512/2871/2871729.png" alt="cuotas" class="iconos">
+          <p class="iconos-texto">Recudación cuotas de mantenimiento</p>
+        </a>
+      </div>
+      
+    </div>
+  </div>
+
+  <br>
+  <div class="container inicio">
+    <div class="flex-inicio">
+      <div class="d-flex flex-column flex-md-row justify-content-between align-items-center">
+        <div class="text-md-start mb-3 mb-md-0 me-md-5">
+          <h2>PLANES ESPECIALIZADOS EN CONDOMINIOS</h2>
+          <p class="explicacion">
+            Ofrecemos tres planes de contratación adaptados a las necesidades de cada condominio. El plan básico, el
+            plan medio y el plan completo
+            Todos nuestros planes tienen descuentos al contratar de forma semestral y anual, lo que permite a los
+            condominios ahorrar dinero y tener una gestión administrativa y de mantenimiento efectiva y eficiente a
+            largo plazo.
+          </p>
+        </div>
+        <div class="text-center">
+          <img src="https://cdn-icons-png.flaticon.com/512/3930/3930204.png" alt="Mantenimiento industrial"
+            class="Satisfaccion" height="150" width="150">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="flex-container">
+      <div class="flex-item">
+        <a href="./servicios/planes/basico.php">
+          <img src="https://cdn-icons-png.flaticon.com/512/263/263115.png" alt="Basico" class="iconos">
+          <p class="iconos-texto">Plan Básico</p>
+        </a>
+      </div>
+      <div class="flex-item">
+        <a href="./servicios/planes/intermedio.php">
+          <img src="https://cdn-icons-png.flaticon.com/512/916/916771.png" alt="Impermeabilización" class="iconos">
+          <p class="iconos-texto">Plan Intermedio</p>
+        </a>
+      </div>
+      <div class="flex-item">
+        <a href="./servicios/planes/avanzado.php">
+          <img src="https://cdn-icons-png.flaticon.com/512/2451/2451655.png" alt="Pintura" class="iconos">
+          <p class="iconos-texto">Plan Servicio Completo</p>
+        </a>
+      </div>
+    </div>
+  </div>
+
+
+
+</body>
+<?PHP require('../footer/footer.php');
+?>
+
+</html>
